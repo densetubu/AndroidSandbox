9fc1ed5a248bb784661c5d114860d4450a4b9d25
ichigotake <k.wisiiy@gmail.com>
2014-03-30 23:29:31 +0900

    AsyncTask vs AsyncTaskLoader(+LoaderCallbacks)

--- Sandbox/src/densetubu/android/sandbox/app/WeatherApiLoaderCallbacks.java
+++ Sandbox/src/densetubu/android/sandbox/app/WeatherApiLoaderCallbacks.java
@@ -6,6 +6,26 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.widget.ArrayAdapter;
 
+/**
+ *
+ * AsyncTask vs AsyncTaskLoader(+LoaderCallbacks)
+ *
+ *  どちらも非同期でバックグラウンド処理が実行されるという点では同じ
+ *  違いはフラグメント・アクティビティに紐づくかどうか
+ *
+ *  アクティビティ・フラグメントでよく実行される処理の場合は、ビューも含めた複数の要素のライフサイクルが複雑に絡み合う
+ *  そのため、適切なキャンセル処理をしないと、参照出来ないビューを扱おうとしてクラッシュが発生したり、
+ *  無駄な処理が続いたままになってしまうケースがある
+ *
+ *  AsyncTaskLoaderの場合はLoaderManagerが適切に管理してくれるインターフェースを提供しており、
+ *  処理のキャンセル忘れが発生するリスクは低い
+ *
+ *  サービスクラス等のLoaderCallbacksが利用出来ない環境では、そもそも自身以外のライフサイクルを意識する必要性が薄い
+ *  AsyncTaskLoaderが使えないからダメという事はなく、サービスクラスでAsyncTaskを使っていてもなんら問題はない
+ *
+ *  とりあえずは、アクティビティ・フラグメント上で実行する場合はAsyncTaskLoader(+LoaderCallbacks)、
+ *  サービスクラスではAsyncTask、と覚えておくとよい
+ */
 class WeatherApiLoaderCallbacks implements LoaderManager.LoaderCallbacks<WeatherForecast> {
 
     private final Context context;
